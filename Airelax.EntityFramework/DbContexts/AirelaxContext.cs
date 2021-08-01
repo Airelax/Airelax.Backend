379841f34a2ab10ef81e4d5d0f2a365d75444977
peace920902 <peace9209021@gmail.com>
2021-08-02 02:03:23 +0800

    SHOP-54 refactor AirelaxContext OnModelCreating method

--- Airelax.EntityFramework/DbContexts/AirelaxContext.cs
+++ Airelax.EntityFramework/DbContexts/AirelaxContext.cs
@@ -46,27 +46,108 @@ namespace Airelax.EntityFramework.DbContexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<House>(
-                builder =>
-                {
-                    builder.SetEntityKey<House,int>();
-                    builder.SetPropMaxLength(x => x.Title, 30).IsRequired();
-                    builder.SetEnumDbMapping(x => x.Status).IsRequired();
-                    builder.SetEnumDbMapping(x => x.CreateState).IsRequired();
-                });
+            ConfigHouse(modelBuilder);
 
-            modelBuilder.Entity<HouseCategory>(builder =>
+            ConfigHouseCategory(modelBuilder);
+
+            ConfigHouseLocation(modelBuilder);
+
+            ConfigHouseDescription(modelBuilder);
+
+            ConfigHouseRule(modelBuilder);
+
+            ConfigPolicy(modelBuilder);
+
+            ConfigReservationRule(modelBuilder);
+
+            ConfigPhoto(modelBuilder);
+
+            ConfigSpace(modelBuilder);
+
+            ConfigBedroomDetail(modelBuilder);
+        }
+
+        private static void ConfigBedroomDetail(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BedroomDetail>(builder =>
             {
-                builder.SetZeroEntityKey<HouseCategory,int>();
-                builder.SetEnumDbMapping(x => x.Category);
-                builder.SetEnumDbMapping(x => x.HouseType);
-                builder.SetEnumDbMapping(x => x.RoomCategory);
-                builder.HasOne<House>().WithOne(x => x.HouseCategory).HasForeignKey<HouseCategory>(x => x.Id);
+                builder.SetEntityKey<BedroomDetail, int>();
+                builder.SetEnumDbMapping(x => x.BedType).IsRequired();
+                builder.HasOne<Space>().WithMany(x => x.BedroomDetail);
             });
+        }
 
+        private static void ConfigSpace(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Space>(builder =>
+            {
+                builder.SetEntityKey<Space, int>();
+                builder.SetEnumDbMapping(x => x.SpaceType).IsRequired();
+                builder.HasOne<House>().WithMany(x => x.Spaces).HasForeignKey(x => x.HouseId);
+            });
+        }
+
+        private static void ConfigPhoto(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Photo>(builder =>
+            {
+                builder.SetEntityKey<Photo, int>();
+                builder.Property(x => x.Image).HasColumnType(Define.SqlServer.IMAGE_TYPE).IsRequired();
+                builder.HasOne<House>().WithMany(x => x.Photos).HasForeignKey(x => x.HouseId).IsRequired();
+                builder.HasOne<Space>().WithMany(x => x.Photos).OnDelete(DeleteBehavior.ClientSetNull).HasForeignKey(x => x.SpaceId);
+            });
+        }
+
+        private static void ConfigReservationRule(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ReservationRule>(builder =>
+            {
+                builder.SetZeroEntityKey<ReservationRule, int>();
+                builder.SetEnumDbMapping(x => x.RejectDate);
+                builder.HasOne<House>().WithOne(x => x.ReservationRule).HasForeignKey<ReservationRule>(x => x.Id);
+            });
+        }
+
+        private static void ConfigPolicy(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Policy>(builder =>
+            {
+                builder.SetZeroEntityKey<Policy, int>();
+                builder.SetEnumDbMapping(x => x.CancelPolicy);
+                builder.Property(x => x.CancelPolicy).HasColumnType(Define.SqlServer.MONEY_TYPE);
+                builder.HasOne<House>().WithOne(x => x.Policy).HasForeignKey<Policy>(x => x.Id);
+            });
+        }
+
+        private static void ConfigHouseRule(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HouseRule>(builder =>
+            {
+                builder.SetZeroEntityKey<HouseRule, int>();
+                builder.SetPropMaxLength(x => x.Other, 500);
+                builder.HasOne<House>().WithOne(x => x.HouseRule).HasForeignKey<HouseRule>(x => x.Id);
+            });
+        }
+
+        private static void ConfigHouseDescription(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HouseDescription>(builder =>
+            {
+                builder.SetZeroEntityKey<HouseDescription, int>();
+                builder.SetPropMaxLength(x => x.Description, 1000);
+                builder.SetPropMaxLength(x => x.SpaceDescription, 1000);
+                builder.SetPropMaxLength(x => x.GuestPermission, 1000);
+                builder.SetPropMaxLength(x => x.Others, 1000);
+                builder.SetEnumDbMapping(x => x.HouseHighlight);
+                builder.HasOne<House>().WithOne(x => x.HouseDescription).HasForeignKey<HouseDescription>(x => x.Id);
+            });
+        }
+
+        private static void ConfigHouseLocation(ModelBuilder modelBuilder)
+        {
             modelBuilder.Entity<HouseLocation>(builder =>
             {
-                builder.SetZeroEntityKey<HouseLocation,int>();
+                builder.SetZeroEntityKey<HouseLocation, int>();
                 builder.SetPropMaxLength(x => x.Country, 50);
                 builder.SetPropMaxLength(x => x.City, 50);
                 builder.SetPropMaxLength(x => x.Town, 50);
@@ -76,62 +157,30 @@ namespace Airelax.EntityFramework.DbContexts
                 builder.SetPropMaxLength(x => x.TrafficDescription, 250);
                 builder.HasOne<House>().WithOne(x => x.HouseLocation).HasForeignKey<HouseLocation>(x => x.Id);
             });
+        }
 
-            modelBuilder.Entity<HouseDescription>(builder =>
+        private static void ConfigHouseCategory(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HouseCategory>(builder =>
             {
-                builder.SetZeroEntityKey<HouseDescription,int>();
-                builder.SetPropMaxLength(x => x.Description, 1000);
-                builder.SetPropMaxLength(x => x.SpaceDescription, 1000);
-                builder.SetPropMaxLength(x => x.GuestPermission, 1000);
-                builder.SetPropMaxLength(x => x.Others, 1000);
-                builder.SetEnumDbMapping(x => x.HouseHighlight);
-                builder.HasOne<House>().WithOne(x => x.HouseDescription).HasForeignKey<HouseDescription>(x => x.Id);
+                builder.SetZeroEntityKey<HouseCategory, int>();
+                builder.SetEnumDbMapping(x => x.Category);
+                builder.SetEnumDbMapping(x => x.HouseType);
+                builder.SetEnumDbMapping(x => x.RoomCategory);
+                builder.HasOne<House>().WithOne(x => x.HouseCategory).HasForeignKey<HouseCategory>(x => x.Id);
             });
+        }
 
-            modelBuilder.Entity<HouseRule>(builder =>
-            {
-                builder.SetZeroEntityKey<HouseRule,int>();
-                builder.SetPropMaxLength(x => x.Other, 500);
-                builder.HasOne<House>().WithOne(x => x.HouseRule).HasForeignKey<HouseRule>(x => x.Id);
-            });
-
-            modelBuilder.Entity<Policy>(builder =>
-            {
-                builder.SetZeroEntityKey<Policy,int>();
-                builder.SetEnumDbMapping(x => x.CancelPolicy);
-                builder.Property(x => x.CancelPolicy).HasColumnType(Define.SqlServer.MONEY_TYPE);
-                builder.HasOne<House>().WithOne(x => x.Policy).HasForeignKey<Policy>(x => x.Id);
-            });
-
-            modelBuilder.Entity<ReservationRule>(builder =>
-            {
-                builder.SetZeroEntityKey<ReservationRule,int>();
-                builder.SetEnumDbMapping(x => x.RejectDate);
-                builder.HasOne<House>().WithOne(x => x.ReservationRule).HasForeignKey<ReservationRule>(x => x.Id);
-            });
-
-            modelBuilder.Entity<Photo>(builder =>
-            {
-                builder.SetEntityKey<Photo,int>();
-                builder.Property(x => x.Image).HasColumnType(Define.SqlServer.IMAGE_TYPE).IsRequired();
-                builder.HasOne<House>().WithMany(x => x.Photos).HasForeignKey(x => x.HouseId).IsRequired();
-                builder.HasOne<Space>().WithMany(x => x.Photos).OnDelete(DeleteBehavior.ClientSetNull).HasForeignKey(x => x.SpaceId);
-            });
-
-            modelBuilder.Entity<Space>(builder =>
-            {
-                builder.SetEntityKey<Space,int>();
-                builder.SetEnumDbMapping(x => x.SpaceType).IsRequired();
-                builder.HasOne<House>().WithMany(x => x.Spaces).HasForeignKey(x => x.HouseId);
-               
-            });
-
-            modelBuilder.Entity<BedroomDetail>(builder =>
-            {
-                builder.SetEntityKey<BedroomDetail,int>();
-                builder.SetEnumDbMapping(x => x.BedType).IsRequired();
-                builder.HasOne<Space>().WithMany(x => x.BedroomDetail);
-            });
+        private static void ConfigHouse(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<House>(
+                builder =>
+                {
+                    builder.SetEntityKey<House,int>();
+                    builder.SetPropMaxLength(x => x.Title, 30).IsRequired();
+                    builder.SetEnumDbMapping(x => x.Status).IsRequired();
+                    builder.SetEnumDbMapping(x => x.CreateState).IsRequired();
+                });
         }
     }
 }
