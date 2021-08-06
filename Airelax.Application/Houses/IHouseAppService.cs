6fe2879ed62c1cb5dad7f73fdbc07d37be5be072
tangshuhan <a0981596982@icloud.com>
2021-08-06 21:35:41 +0800

    SHOP-56 Search House API

---
+++ Airelax.Application/Houses/IHouseAppService.cs
@@ -0,0 +1,15 @@
+﻿using Airelax.Application.Houses.Dtos.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airelax.Application.Houses
+{
+    public interface IHouseAppService
+    {
+        Task<HouseDto> GetHouse(int id);
+
+    }
+}
