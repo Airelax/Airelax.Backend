3a105ddc6b1609ff4d02bde950d93c980239ef54
peace920902 <peace9209021@gmail.com>
2021-08-08 22:20:05 +0800

    fix spell error

--- Airelax.Domain/Houses/Defines/Facility.cs
+++ Airelax.Domain/Houses/Defines/Facility.cs
@@ -38,7 +38,7 @@
         Audio = 309, //音響
         Television = 310,
 
-        // 4xx Famlily
+        // 4xx Family
         BabyBath = 401, //嬰兒浴盆
         BabyMonitor = 402, //嬰兒監視器
         BabySafetyGates = 403, //嬰兒安全門欄
@@ -50,18 +50,18 @@
         Crib = 409, //嬰兒床
         FireplaceGuards = 410, //壁爐護欄
         HighChair = 411, //兒童高腳椅
-        OutletCovers =412, // 插座蓋
+        OutletCovers = 412, // 插座蓋
         TravelCrib = 413, // 旅行用嬰兒床
         TableCornerGuards = 414, // 桌腳防護
         WindowGuards = 415, //窗柵
 
         // 5xx AirConditioner
-        
+
         AirConditioner = 501, //空調設備
         CeilingFan = 502, //吊扇
         Heating = 503, //暖氣
         IndoorFireplace = 504, //室內壁爐
-        PortableFans = 505 , //移動式電扇
+        PortableFans = 505, //移動式電扇
 
         // 6xx Safety
         CarbonMonoxideAlarm = 601, //一氧化碳警報器
@@ -76,28 +76,28 @@
         Wifi = 703,
 
         //8xx Kitchen
-        Bakingsheet = 801, //烤盤
+        BakingSheet = 801, //烤盤
         BarbecueUtensils = 802, //燒烤用具
-        BreadMaker = 803, 
+        BreadMaker = 803,
         Blender = 804, //調理機
-        Coffee = 805, 
+        Coffee = 805,
         CoffeeMaker = 806,
         CookingBasics = 807, //基本廚具
         DiningTable = 808,
         DishesAndSilverware = 809,
         Dishwasher = 810,
         Freezer = 811,
-        HotWaterKettle = 812,//熱水壺
-        Kitchen = 813, 
+        HotWaterKettle = 812, //熱水壺
+        Kitchen = 813,
         Microwave = 814,
-        MiniFridge =815,
+        MiniFridge = 815,
         Oven = 816, //烤箱
         Refrigerator = 817,
         RiceMaker = 818,
-        Stove = 814, //爐子
-        Toaster = 815, 
-        TrashCompactor = 816, //壓縮式垃圾桶
-        WineGlasses = 817, //葡萄酒杯
+        Stove = 819, //爐子
+        Toaster = 820,
+        TrashCompactor = 821, //壓縮式垃圾桶
+        WineGlasses = 822, //葡萄酒杯
 
         //9xx Location features
         BeachAccess = 901, //直達海灘
@@ -105,34 +105,34 @@
         LaundromatNearby = 903, //附近有自助洗衣店
         PrivateEntrance = 904, //獨立入口
         ResortAccess = 905, //渡假村入場證
-        Skiout = 906,//無須搭乘交通工具即可前往滑雪場
+        SkiOut = 906, //無須搭乘交通工具即可前往滑雪場
         Waterfront = 907, //緊鄰水岸
-        
+
         //10xx outdoor 
         Backyard = 1001, //後院
         BBQ = 1002, //烤肉區
-        BeachEssentials =1003, //沙灘必備物品
+        BeachEssentials = 1003, //沙灘必備物品
         Bikes = 1004,
         BoatSlip = 1005, //船梯
         FirePit = 1006, //火坑
         Hammock = 1007, //吊床
         Kayak = 1008, //小輕艇
-        OutdoorDiningArea = 1009,//室外用餐區
+        OutdoorDiningArea = 1009, //室外用餐區
         OutdoorFurniture = 1010, //戶外家具
         OutdoorKitchen = 1011,
         Patio = 1012, //庭院
 
         //11xx Parking and other
         Elevator = 1101,
-        EVcharger = 1102, // 電動汽車充電設備
+        EVCharger = 1102, // 電動汽車充電設備
         FreeBuildingParking = 1103, //建築物內免費停車
-        FreeStreetParking = 1104 ,  //路邊有免費停車
+        FreeStreetParking = 1104, //路邊有免費停車
         Gym = 1105, //健身房
         Jacuzzi = 1106, //按摩浴缸
         PaidParkingOffPremises = 1107, //建築物範圍外有收費停車
         PaidParkingOnPremises = 1108, //建築物內有收費停車位
         Pool = 1109,
-        PrivateLivingRoom = 1110,//獨立客廳
+        PrivateLivingRoom = 1110, //獨立客廳
         Sauna = 1111, //三溫暖
         SingleLevelHome = 1112, //獨立房屋
 
@@ -140,7 +140,6 @@
         Breakfast = 1201,
         CleaningBeforeCheckout = 1202, // 退房前打掃
         LongTermStaysAllowed = 1203, //可長期住宿
-        LuggageDropoffAllowed = 1204, //可存放行李
-
+        LuggageDropOffAllowed = 1204, //可存放行李
     }
 }
