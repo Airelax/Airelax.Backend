1faa994ab1819c5fd86f285cd1eaa8a89659569b
peace920902 <peace9209021@gmail.com>
2021-08-23 17:12:07 +0800

    merge new version frontend

--- Airelax.Frontend/src/router/index.js
+++ Airelax.Frontend/src/router/index.js
@@ -15,7 +15,20 @@ const routes = [
     path: '/room',
     name: 'Room',
     component: () => import('../views/Room.vue')
+  },  
+  {
+    path: '/subscribe',
+    name: 'Subscribe',
+    component: () => import('../views/Subscribe.vue')
   },
+  {
+    path: '/new-house',
+    name: 'NewHouse',
+    component: () => import('../views/NewHouse/NewHouse.vue'),
+    meta: {
+      plainLayout: true,
+    },
+  }
 ]
 
 const router = createRouter({
