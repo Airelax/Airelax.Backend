169f03e6ef2fdc9ae82ea5162449cc5553c5f634
Girro39 <yellyou39@gmail.com>
2021-08-26 16:24:35 +0800

    SHOP-68 抽完Service Repository

--- Airelax/Controllers/MemberInfoController.cs
+++ Airelax/Controllers/MemberInfoController.cs
@@ -14,81 +14,26 @@ namespace Airelax.Controllers
     [Route("[controller]")]
     public class MemberInfoController : Controller
     {
-        private readonly MemberInfoRepository _memberInfoRepository;
-        private readonly AirelaxContext _context;
+        private readonly IMemberInfoService _memberInfoService;
 
-        public MemberInfoController(AirelaxContext context, MemberInfoRepository memberInfoRepository)
+        public MemberInfoController(IMemberInfoService memberInfoService)
         {
-            _context = context;
-            _memberInfoRepository = memberInfoRepository;
+            _memberInfoService = memberInfoService;
         }
 
-
-        //private readonly AirelaxContext _context;
-        //public MemberInfoController(AirelaxContext context)
-        //{
-        //    _context=context ;
-        //}
 
         [HttpGet]
         [Route("{memberId}")]
         public IActionResult Index(string memberId)
         {
-            var info =
-                (from member in _context.Members
-                 from contextMemberInfo in _context.MemberInfos.Where(x => x.Id == member.Id).DefaultIfEmpty()
-                 from contextHouse in _context.Houses.Where(x => x.OwnerId == member.Id).DefaultIfEmpty()
-                 from contextPhoto in _context.Photos.Where(x => x.HouseId == contextHouse.Id).DefaultIfEmpty()
-                 from contextHouseCategory in _context.HouseCategories.Where(x => x.Id == contextHouse.Id).DefaultIfEmpty()
-                 from contextComment in _context.Comments.Where(x => x.HouseId == contextHouse.Id).DefaultIfEmpty()
-                 from contextStar in _context.Stars.Where(x => x.Id == contextComment.Id).DefaultIfEmpty()
-                 where member.Id == memberId && !member.IsDeleted
-                 select new
-                 {
-                     About = contextMemberInfo.About,
-                     Location = contextMemberInfo.Location,
-                     WorkTime = contextMemberInfo.WorkTime,
-                     MemberName = member.Name,
-                     RegisterTime = member.RegisterTime,
-                     Email = member.Email,
-                     HouseTitle = contextHouse.Title,
-                     HouseType = contextHouseCategory.HouseType,
-                     RoomType = contextHouseCategory.RoomCategory,
-                     StarTotal = contextStar,
-                     CommentHouseId = contextComment.HouseId,
-                     HouseId = contextHouse.Id
-                     //todo 會員相片 跟 房屋相片
-                 }).ToList();
+            var memberInfoViewModel = _memberInfoService.GetMemberInfoViewModel(memberId);
 
-
-            var groupInfo = info.GroupBy(x => x.HouseId);
-            var memberInfoViewModel = new MemberInfoViewModel()
-            {
-                MemberId = memberId,
-                About = info.First().About,
-                Location = info.First().Location,
-                WorkTime = info.First().WorkTime,
-                MemberName = info.First().MemberName,
-                RegisterTime = info.First().RegisterTime.ToString("yyyy"),
-                Email = info.First().Email,
-                //todo 會員相片
-                HouseSource = info.Select
-                (x => new MemberInfoHouseDto
-                {
-                    HouseId = x.HouseId,
-                    CommentCount = groupInfo.Count(xc => xc.Key == x.HouseId),
-                    HouseType = x.HouseType.ToString(),
-                    RoomType = x.RoomType.ToString(),
-                    RoomTitle = x.HouseTitle,
-                    StarScore = x.StarTotal?.Total
-                    //todo 房屋相片
-                })
-            };
-            if (info.IsNullOrEmpty())
+            if (memberInfoViewModel == null)
             {
                 //todo 錯誤畫面
                 return Content("錯誤畫面");
             }
+
             return View(memberInfoViewModel);
         }
 
@@ -97,35 +42,9 @@ namespace Airelax.Controllers
         [Route("{memberId}")]
         public async Task<MemberInfoInput> UpdateMemberInfo(string memberId, [FromBody] MemberInfoInput input)
         {
+            var aboutMe = _memberInfoService.GetAboutMe(memberId, input);
 
-            var member = (from m in _context.Members
-                          from mi in _context.MemberInfos.Where(x => x.Id == m.Id).DefaultIfEmpty()
-                          where m.Id == memberId
-                          select new { Member = m, MemberInfos = mi }).FirstOrDefault();
-
-            if (member == null)
-                throw ExceptionBuilder.Build(System.Net.HttpStatusCode.BadRequest
-                    , $"Member Id:{memberId} does not match any member");//404
-
-            if (member?.MemberInfos == null)
-            {
-                var memberInfo = new MemberInfo(memberId)
-                {
-                    About = input.About,
-                    Location = input.Location,
-                    WorkTime = input.WorkTime
-                };
-                _memberInfoRepository.Add(memberInfo);
-            }
-            else
-            {
-                member.MemberInfos.About = input.About;
-                member.MemberInfos.Location = input.Location;
-                member.MemberInfos.WorkTime = input.WorkTime;
-                _memberInfoRepository.Update(member.MemberInfos);
-            }
-            _memberInfoRepository.SaveChanges();
-            return input;
+            return aboutMe;
         }
 
 
