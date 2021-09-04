7ee3071c450612d9744bfca5f9f4f49890987767
tangshuhan <a0981596982@icloud.com>
2021-09-05 00:00:08 +0800

    SHOP-90 Finish

---
+++ Airelax/Controllers/MyHousesController.cs
@@ -0,0 +1,32 @@
+﻿using Airelax.Application.Members.Request;
+using Airelax.Domain.Members;
+using Airelax.EntityFramework.DbContexts;
+using Lazcat.Infrastructure.ExceptionHandlers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airelax.Controllers
+{
+    [Route("[controller]")]
+    public class MyHousesController : Controller
+    {
+        private readonly IMyHousesService _myHousesService;
+        public MyHousesController(IMyHousesService myHousesService)
+        {
+            _myHousesService = myHousesService;
+
+        }
+        [HttpGet]
+        [Route("{ownerId}")]
+        public IActionResult MyHousesDetail(string ownerId)
+        {
+            var myhousesViewModel = _myHousesService.GetMyHouseViewModel(ownerId);
+            return View(myhousesViewModel);
+
+        }
+    }
+}
---
+++ Airelax/IMyHousesRepository.cs
@@ -0,0 +1,10 @@
+﻿using Airelax.Domain.Houses;
+using System.Collections.Generic;
+
+namespace Airelax
+{
+    public interface IMyHousesRepository
+    {
+        List<House> GetHousesByOwnerId(string ownerId);
+    }
+}
