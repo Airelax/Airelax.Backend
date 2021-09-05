dfc331030d917c9325fe01b74d3345b4d773c8ac
MSI\USER <kk850408@gmail.com>
2021-09-05 15:59:07 +0800

    渲染房源

--- Airelax/MyHousesService.cs
+++ Airelax/MyHousesService.cs
@@ -1,4 +1,5 @@
-﻿using Lazcat.Infrastructure.DependencyInjection;
+﻿using Airelax.Domain.Houses.Defines;
+using Lazcat.Infrastructure.DependencyInjection;
 using Lazcat.Infrastructure.Extensions;
 using System;
 using System.Collections.Generic;
@@ -20,19 +21,59 @@ namespace Airelax
             var myhouses = _myHousesRepository.GetHousesByOwnerId(OwnerId);
 
             if (myhouses.IsNullOrEmpty())
-                return new List<MyHouseViewModel>();
-            
+                return new List<MyHouseViewModel>() {
+                    new MyHouseViewModel
+                    {
+                        Title = "傑哥的房子",
+                        HouseStatus = HouseStatus.Publish,
+                        CreateState = CreateState.Building,
+                        CanRealTime = false,
+                        Location = "竹北，新竹",
+                        LastReservationTime = DateTime.Now.ToString("yyyy-MM-dd")
+                    },
+                     new MyHouseViewModel
+                    {
+                        Title = "傑哥的房子",
+                        HouseStatus = HouseStatus.Publish,
+                        CreateState = CreateState.Building,
+                        CanRealTime = false,
+                        Location = "竹北，新竹",
+                        LastReservationTime = DateTime.Now.ToString("yyyy-MM-dd")
+                     },
+                      new MyHouseViewModel
+                    {
+                        Title = "我房間的標題超級無敵超窩窩窩窩窩窩喔窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩窩",
+                        HouseStatus = HouseStatus.Publish,
+                        CreateState = CreateState.Completed,
+                        CanRealTime = false,
+                        Location = "竹北，新竹",
+                        LastReservationTime =DateTime.Now.ToString("yyyy-MM-dd")
+                    },
+                       new MyHouseViewModel
+                    {
+                        
+                        Title = "傑哥的房子",
+                        HouseStatus = HouseStatus.Publish,
+                        CreateState = CreateState.Building,
+                        CanRealTime = false,
+                        Location = "竹北，新竹",
+                        LastReservationTime =DateTime.Now.ToString("yyyy-MM-dd")
+                    }
+
+                };
+
             var myhouseViewModel = myhouses.Select(x => new MyHouseViewModel()
             {
+                HouseId = x.Id,
                 Title = x.Title,
                 HouseStatus = x.Status,
                 CreateState = x.CreateState,
                 CanRealTime = x.Policy.CanRealTime,
                 Location = $"{x.HouseLocation.City},{ x.HouseLocation.Country }",
-                LastReservationTime = x.ReservationRule.LastReservationTime
+                LastReservationTime = x.ReservationRule.LastReservationTime.ToString("yyyy-MM-dd")
             });
 
-            return myhouseViewModel;
+            return null;
         }
 
     }
