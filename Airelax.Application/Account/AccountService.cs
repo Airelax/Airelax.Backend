0ee2b8a36db6122612e17fed7e5310f753cc052c
peace920902 <peace9209021@gmail.com>
2021-09-05 17:15:01 +0800

    account repository

--- Airelax.Application/Account/AccountService.cs
+++ Airelax.Application/Account/AccountService.cs
@@ -14,6 +14,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
 using Airelax.Application.Account.Dtos.Response;
+using Airelax.Domain.RepositoryInterface;
 using Microsoft.Extensions.Configuration;
 
 namespace Airelax
