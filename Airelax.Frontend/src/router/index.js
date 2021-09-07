5efb5f83010d8625e7304bd4c90dc2c516955806
BS-Howard <howard199887@gmail.com>
2021-09-07 17:06:21 +0800

    new

--- Airelax.Frontend/src/router/index.js
+++ Airelax.Frontend/src/router/index.js
@@ -1,39 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
 
 const routes = [
-<<<<<<< HEAD
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue'),
-  },
-  {
-    path: '/search',
-    name: 'Search',
-    component: () => import('../views/Search.vue')
-  },
-  {
-    path: '/room',
-    name: 'Room',
-    component: () => import('../views/Room.vue')
-  },
-  {
-    path: '/subscribe',
-    name: 'Subscribe',
-    component: () => import('../views/Subscribe.vue')
-  },
-  {
-    path: '/wishList',
-    name: 'wishList',
-    component: () => import('../views/WishList.vue')
-  },
-  {
-    path: '/new-house',
-    name: 'NewHouse',
-    component: () => import('../views/NewHouse/NewHouse.vue'),
-    meta: {
-      plainLayout: true,
-=======
     {
         path: '/',
         name: 'Home',
