e9c37326e97f19549d1082849d2efac8db6bcddf
BS-Howard <howard199887@gmail.com>
2021-09-07 16:58:19 +0800

    new

--- Airelax.Frontend/src/router/index.js
+++ Airelax.Frontend/src/router/index.js
@@ -17,6 +17,11 @@ const routes = [
     component: () => import('../views/Room.vue')
   },
   {
+    path: '/message',
+    name: 'Message',
+    component: () => import('../views/Information.vue')
+  },
+  {
     path: '/subscribe',
     name: 'Subscribe',
     component: () => import('../views/Subscribe.vue')
---
+++ Airelax.Frontend/src/views/Information.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>23</h1>
+</template>
+
+<script>
+// import axios from "axios";
+export default {
+    
+}
+</script>
