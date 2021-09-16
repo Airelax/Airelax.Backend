0a68deb2c9c29fef7708eac95215ae500eec626a
tangshuhan <a0981596982@icloud.com>
2021-09-16 21:37:50 +0800

    SHOP-114 Trip

---
+++ Airelax/ITripService.cs
@@ -0,0 +1,10 @@
+﻿using Airelax.Application.Houses.Dtos.Response;
+using System.Threading.Tasks;
+
+namespace Airelax
+{
+    public interface ITripService
+    {
+        Task<TripViewModels> GetTripViewModel();
+    }
+}
